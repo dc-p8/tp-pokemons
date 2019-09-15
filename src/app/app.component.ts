f7f7059b042d57b0fbdafbb48edc2ed226334af8
dc <cardinal.dorian@gmail.com>
2019-09-15 07:43:56 +0200

    responsive layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,25 @@
-import { Component } from '@angular/core';
+import {
+	Component, OnInit
+} from '@angular/core';
+import {
+	BreakpointObserver
+} from '@angular/cdk/layout';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+	items = [...Array(100).keys()].map((item => `item ${item}`));
+	isMobile:boolean = true;
+	ngOnInit(){
+		this.bpo.observe('(max-width: 600px)').subscribe((state) => {
+			this.isMobile = state.matches
+		})
+	}
+	constructor(
+		private bpo: BreakpointObserver) {
+		
+	}
 }
